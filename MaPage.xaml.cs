1e881d5379d4d711021c4d05bbbf3bed422f4af8
xuchi <xuchi0601@qq.com>
2022-10-07 14:54:30 +0800

    init

---
+++ MaPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauApp;
+
+public partial class MaPage : ContentPage
+{
+	public MaPage()
+	{
+		InitializeComponent();
+        SocialMedias = new ObservableCollection<SocialMedia>
+        {
+            new SocialMedia() { Name = "Facebook", ID = 0 },
+            new SocialMedia() { Name = "Google Plus", ID = 1 }
+        };
+    }
+    public ObservableCollection<SocialMedia> SocialMedias { get; set; } = new ObservableCollection<SocialMedia>();
+}
+
+public class SocialMedia
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using Syncfusion.Maui.Core.Hosting;
+using Syncfusion.Maui.ListView.Hosting;
+
+namespace MauApp;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+        .UseMauiApp<App>()
+        .ConfigureSyncfusionCore()
+        .ConfigureSyncfusionListView();
+
+        return builder.Build();
+    }
+}
